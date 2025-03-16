b126b5cfd2ce5d64ccc8cdd0ad7467fb0165e460
DaniielDz <daniel13cba2002@gmail.com>
2025-03-16 14:41:37 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import Header from "./layout/Header"
+
+function App() {
+
+  return (
+    <>
+    <Header />
+    <main>
+      
+    </main>
+    <footer>
+
+    </footer>
+    </>
+  )
+}
+
+export default App
